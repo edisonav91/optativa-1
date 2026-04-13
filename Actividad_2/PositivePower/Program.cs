09b715b4bd064f4a1910e9cace84cf4ebff33ce5
Edison Alvarez <edisonav1991@gmail.com>
2026-04-12 23:50:55 -0500

    16. Ordena Positive Power y agrega gitignore

--- Actividad_2/PositivePower/Program.cs
+++ Actividad_2/PositivePower/Program.cs
@@ -2,16 +2,16 @@ using System;
 
 // Name: Positive Power
 // Titulo: Potencia positiva
-// Se debe hacer: Pedir un numero y elevarlo al cuadrado si es positivo.
+// Descripcion: Solicita al usuario un numero y eleva este numero al cuadrado solo si es positivo.
 
 Console.Write("Ingrese un numero: ");
-double numero = Convert.ToDouble(Console.ReadLine());
+double valor = double.Parse(Console.ReadLine());
 
-if (numero > 0)
+if (valor > 0)
 {
-    Console.WriteLine("Resultado: " + (numero * numero));
+    Console.WriteLine("Resultado: " + (valor * valor));
 }
-else if (numero == 0)
+else if (valor == 0)
 {
     Console.WriteLine("Resultado: 0");
 }
