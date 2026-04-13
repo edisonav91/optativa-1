2a77046aaabbf8018ca66e18521491e9a7ed54d4
Edison Alvarez <edisonav1991@gmail.com>
2026-04-12 22:10:39 -0500

    10.Finalizado algoritmo String Length

---
+++ Actividad_2/StringLength/Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+// Name: String Length
+// Titulo: Longitud de una palabra
+// Descripcion: Pide una palabra al usuario y muestra la longitud de esa palabra.
+
+Console.Write("Ingrese una palabra: ");
+string texto = Console.ReadLine();
+
+Console.WriteLine("Resultado: " + texto.Length);
